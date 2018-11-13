ecf1089b3e6b72a4eed9d053fe72f1c046770fb4
santirossi21 <santi.rossi@outlook.com.ar>
2018-11-13 13:02:33 -0300

    Cambios de archivos por los del juego

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,124 +1,125 @@
 
-
 GamePlayManager = {
-
-    
-
     //init == awake (unity).
     init: function() {
         console.log("init");
+
     },
     //Carga todos los assets y variables.
     preload: function() {
         console.log("preload");
-        var horse;
-        var mollusk;
         var background;
-        var shark;
-        var diamonds;
+        var pelota;
+        var carlos;
+        var pity;
+        var cursors;
+        var nKey;
+        var aKey;
+        var dKey;
+        var mKey;
+        var spaceKey;
+        var cKey;
+        game.load.image("background", "assets/images/background.png");
+        game.load.image("pelota", "assets/images/pelota.png");
+        game.load.image("carlos", "assets/images/carlos.png");
+        game.load.image("pity", "assets/images/pity.png");
 
-        //game.load.image('horse', 'assets/images/horse.png');
-        game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2);
-        game.load.spritesheet('diamonds', 'assets/images/diamonds.png', 81, 84, 4);
-        game.load.image('background', 'assets/images/background.png');
-        game.load.image('mollusk', 'assets/images/mollusk.png');
-        game.load.image('shark', 'assets/images/shark.png');
     },
     //create == start (unity).
     create: function() {
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        //game.physics.arcade.gravity.y = 200;
-
         console.log("create");
+
         this.background = game.add.sprite(0, 0, 'background');
 
+        //  Enable p2 physics
+        game.physics.startSystem(Phaser.Physics.P2JS);
 
-        this.mollusk = game.add.sprite(100, 10, 'mollusk');
-        this.shark = game.add.sprite(100, 100, 'shark');
+        game.physics.p2.gravity.y = 300;
 
-        this.horse = game.add.sprite(512, 384, 'horse',1);
-        this.horse.anchor.setTo(.5);
-        this.horse.animations.add('walk');
-        this.horse.animations.play('walk', 2, true);
+        var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial');
+        var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
+        var contactMaterial = game.physics.p2.createContactMaterial(spriteMaterial, worldMaterial, { restitution: 1.0 });
 
-        this.diamonds = game.add.sprite(700, 184, 'diamonds',1);
+        game.physics.p2.setWorldMaterial(worldMaterial);
 
-        
+        this.carlos = game.add.sprite(100, 100, 'carlos');
+        this.pity = game.add.sprite(800, 400, 'pity');
+        this.pelota = game.add.sprite(100, 400, 'pelota');
 
-        this.game.physics.arcade.enable(this.horse, Phaser.Physics.ARCADE);
-        this.game.physics.arcade.enable(this.diamonds, Phaser.Physics.ARCADE);
+        //  Enable for physics. This creates a default rectangular body.
+        game.physics.p2.enable([ this.pelota ]);
+        game.physics.p2.enable([ this.carlos ]);
+        game.physics.p2.enable([ this.pity ]);
 
-         this.horse.body.collideWorldBounds = true;
-        // this.horse.enableBody = true;
+        this.pelota.body.setMaterial(spriteMaterial);
+        this.carlos.body.setMaterial(spriteMaterial);
+        this.pity.body.setMaterial(spriteMaterial);
 
-        // this.horse.body.checkCollision.RIGHT = false;
-        // this.horse.body.checkCollision.LEFT = false;
-        // this.horse.body.bounce.setTo(100, 1);
+        //  Escala de gravedad
+        this.pelota.body.data.gravityScale = 1,5;
+        this.carlos.body.data.gravityScale = 9;
+        this.pity.body.data.gravityScale = 9;
 
-        // this.diamonds.body.collideWorldBounds = true;
-        // this.diamonds.body.immovable = true;
-        // this.diamonds.body.checkCollision.LEFT = true;
-        // this.diamonds.body.checkCollision.DOWN = true;
+        // Movimiento Flechas
+        cursors = game.input.keyboard.createCursorKeys();
 
-        // this.horse.body.bounce.set(1);
-    
-        //  this.diamonds.body.collideWorldBounds = true;
-        //  this.diamonds.body.immovable = true;
-        //  this.diamonds.body.allowGravity = false;
+        //  Registro de letras.
+        this.aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+        this.dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+        this.wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        this.nKey = game.input.keyboard.addKey(Phaser.Keyboard.N);
+        this.mKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
+        this.cKey = game.input.keyboard.addKey(Phaser.Keyboard.C);
+        this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     },
     //Frame a Frame.
     update: function() {
-        //console.log("update");
-        //this.game.physics.arcade.collide(this.horse, this.diamonds);
+        console.log("update");
+        
+        this.carlos.body.velocity.x = 0;
+        this.carlos.body.velocity.y = 500;
+        this.pity.body.velocity.x = 0;
+        this.pity.body.velocity.y = 500;
 
-        this.game.physics.arcade.overlap(
-            this.horse, 
-            this.diamonds, 
-            this.collisionHandler, 
-            null, 
-            this
-        );
-
-        // if (this.game.physics.arcade.collide(this.horse, this.diamonds))
-        // {
-        //     console.log("HACE ALGO");
-        // }
-
-
-        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+        // Controles Pity (JUGADOR 1)
+        if (this.aKey.isDown)
         {
-            //console.log("left");
-            this.horse.scale.x = -1
-            this.horse.x -= 4;
-            
+            this.pity.body.moveLeft(200);
         }
-        else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+        else if (this.dKey.isDown)
         {
-            this.horse.x += 4;
-            this.horse.scale.x = 1
+            this.pity.body.moveRight(200);
+        }
+        if (this.spaceKey.isDown)
+        {
+            this.pity.body.moveUp(1200);
+        }
+        if (this.cKey.isDown)
+        {
+        }
+
+        // Controles Carlos (JUGADOR 2)
+        if (cursors.left.isDown)
+        {
+            this.carlos.body.moveLeft(200);
+        }
+        else if (cursors.right.isDown)
+        {
+            this.carlos.body.moveRight(200);
+        }
+        if (this.nKey.isDown)
+        {
+            this.carlos.body.moveUp(1200);
+        }
+        if (this.mKey.isDown)
+        {
         }
     
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
-        {
-            this.horse.y -= 4;
-        }
-        else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-        {
-            this.horse.y += 4;
-        }
     },
-
-    collisionHandler: function(obj1, obj2) {
-        //console.log("print")
-        this.diamonds.kill();
-    }
 }
 
-
-
 var game = new Phaser.Game(1024, 768, Phaser.CANVAS);
-
-
+    
 game.state.add("gameplay", GamePlayManager);
 game.state.start("gameplay");
